06b3ba4bfa84525210213f1a71771244946d0bc9
Chris Anderson <jchris@apache.org>
2010-06-11 22:36:29 -0700

    add some ui hacking

--- evently/items/list/data.js
+++ evently/items/list/data.js
@@ -2,8 +2,10 @@ function(data) {
   $.log(data)
   var p;
   return {
+    // move this code to the view for better performance due to less data on disk.  
     items : data.rows.map(function(r) {
       p = r.value.profile;
+      p.rev = r.value._rev;
       p.message = r.value.message;
       p.state = r.value.state || "open";
       p.id = r.id;
