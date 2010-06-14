b67d04340377d52fd30ba3ed02d8c4612645ecf1
Chris Anderson <jchris@apache.org>
2010-06-14 14:18:00 -0700

    tag clouds sorta

--- evently/items/cloud/data.js
+++ evently/items/cloud/data.js
@@ -1,10 +1,11 @@
-function(resp) {
+function(resp, e, params) {
   return {
     cloud : resp.rows.map(function(r) {
       var word = r.key.pop();
       return {
         word : word,
         // todo use a new mustache delimiter for uri-encode
+        user_uri : encodeURIComponent(params.user),
         word_uri : encodeURIComponent(word),
         size : (r.value * 4) + 10
       };
