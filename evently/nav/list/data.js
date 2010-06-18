b286e29de2f41f9f2803cbdcce16102fe9e2f797
Chris Anderson <jchris@apache.org>
2010-06-18 10:43:52 -0700

    better linking

--- evently/nav/list/data.js
+++ evently/nav/list/data.js
@@ -1,9 +1,13 @@
 function(e, params) {
   var me = $$("#account").userCtx.name, view = {
-    user : params && params.user || "_all"    
+    user : (params && params.user) || "_all",
+    focus : "Focus"
   };
   if (me && me != params.user) {
     view.me = me;
+  }
+  if (view.user != "_all") {
+    view.focus = "Items";
   }
   return view;
 };
