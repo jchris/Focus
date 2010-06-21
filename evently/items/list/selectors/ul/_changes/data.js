d20f83645cbeb9615b017892741d2f78a5e3cd48
Chris Anderson <jchris@apache.org>
2010-06-21 16:25:11 -0700

    one click to set state

--- evently/items/list/selectors/ul/_changes/data.js
+++ evently/items/list/selectors/ul/_changes/data.js
@@ -1,6 +1,17 @@
 function(data, e) {
   var p, params = e.data.args[1], name;
   
+  var states = ["open","active","done","blocked","someday"];
+  
+  function selectStates(state) {
+    return $.map(states, function(s) {
+      return {
+        state : s,
+        selected : s == state && "selected"
+      }
+    })
+  };
+  
   function newName(n) {
     if (n == name) {
       return false;
@@ -19,7 +30,9 @@ function(data, e) {
       p.rev = doc._rev;
       p.message = $.linkify($.mustache.escape(doc.message));
       p.state = doc.state || "open";
+      p.states = selectStates(p.state);
       p.id = r.id;
+      p.rev = doc._rev;
       p.pic = n;
       p.npic = !n;
       p.publish = !doc.publish && "noshare";
---
+++ evently/items/list/selectors/ul/_changes/selectors/select[name=state]/change.js
@@ -0,0 +1,21 @@
+function() {
+  var select = $(this), 
+    state = select.val(), 
+    li = select.parents("li"), 
+    rev = li.attr("data-rev"),
+    id = li.attr("data-id");
+  $$(select).app.db.openDoc(id, {
+    success : function(doc) {
+      doc.state = state;
+      doc._rev = rev;
+      doc.edit_at = new Date();
+      doc.edit_by = $$("#account").userCtx.name;
+      $$(select).app.db.saveDoc(doc, {
+        success : function(r) {
+          $("#profile .saved").html('<span>Updated item: </span><a href="#/details/'+r.id+'">'+doc.message+'</a> ');
+        }
+      });
+    }
+  });
+  
+};
