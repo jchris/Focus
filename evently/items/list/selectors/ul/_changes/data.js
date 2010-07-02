317b8f864c267e3ca0c9039d507d44634d2e65d5
Chris Anderson <jchris@apache.org>
2010-07-01 22:21:05 -0700

    make pics show up on every item all the time

--- evently/items/list/selectors/ul/_changes/data.js
+++ evently/items/list/selectors/ul/_changes/data.js
@@ -1,24 +1,13 @@
 function(data, e) {
-  var p, params = e.data.args[1], name;
-  
-  
+  var p, params = e.data.args[1];
+
   var slib = $$(this).app.require("lib/states");
 
-  
-  function newName(n) {
-    if (n == name) {
-      return false;
-    } else {
-      name = n;
-      return true;
-    }
-  }
-  
   return {
     // move this code to the view for better performance due to less data on disk.  
     gravatar_url : data.rows[0] && data.rows[0].value.profile.gravatar_url,
     items : $.map(data.rows, function(r) {
-      var doc = r.value, n = newName(doc.profile.name);
+      var doc = r.value;
       p = doc.profile;
       p.rev = doc._rev;
       p.message = $.linkify($.mustache.escape(doc.message));
@@ -26,8 +15,6 @@ function(data, e) {
       p.states = slib.selectStates(p.state);
       p.id = r.id;
       p.rev = doc._rev;
-      p.pic = n;
-      p.npic = !n;
       p.publish = !doc.publish && "noshare";
       p.blocked = doc.blocked && "blocked";
       
