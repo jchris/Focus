483c711da4f3d787f86e9f1760d5e0518d89c7ba
Chris Anderson <jchris@apache.org>
2010-07-11 18:58:27 -0700

    redraw lists on logout and login

--- evently/items/team/after.js
+++ evently/items/team/after.js
@@ -1,17 +1,9 @@
 function(e, p) {
-  // set app to connect to changes with include docs (or support upgrade...)
-  // filter js is a function to say whether or not you care about an update...
-  // maybe there is a way to have it interact with startkey - endkey and the view definition?
-  // autofilter for query json
-  
   var ul = $(this), app = $$(ul).app;
   $.evently.followChanges(app);
-  $("body").bind("evently.changes."+app.db.name, function(e, resp) {
+  $("body").bind("evently-changes-"+app.db.name, function(e, resp) {
     $.each(resp.results, function() {
       $('li[data-user="'+this.doc.profile.name+'"].user ul', ul).trigger("_init");
     });
   });
-  // connect to changes
-  // parse the ids
-  // trigger dom elements that match them
 };
